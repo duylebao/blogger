25700c858f93f4c0b7759e8dd59cfb7db29b1a9d
Duy Le <dle@walmartlabs.com>
2015-09-04 10:54:49 -0700

    bring in routes

--- index.js
+++ index.js
@@ -1,138 +1,159 @@
+let express = require('express');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+let cookieParser = require('cookie-parser');
+let session = require('express-session');
+let passport = require('passport');
+let LocalStrategy = require('passport-local').Strategy;
+let nodeifyit = require('nodeifyit');
+let crypto = require('crypto');
+let flash = require('connect-flash');
 let mongoose = require('mongoose');
 let User = require('./user');
 
-mongoose.connect('mongodb://localhost:27017/authenticator');
+mongoose.connect('mongodb://127.0.0.1:27017/demo');
 
-let user = new User();
-user.username = 'DuyLe';
-user.email = 'dle5@walmart.com';
-user.password = 'test5D';
+// Will allow crypto.promise.pbkdf2(...)
+require('songbird');
 
+const NODE_ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 8000;
+const SALT = 'salt';
+
+let app = express();
 
 process.on('uncaughtException', function(err) {
-    console.log('uncaughtException: \n\n', err.stack)
+    console.log('uncaughtException: \n\n', err.stack);
     // IMPORTANT! (optionally, soft exit)
-    process.exit()
-})
+    process.exit();
+});
 
 process.on('unhandledRejection', (err, rejectedPromise) => {
-    console.log('unhandledRejection: \n\n', err.stack)
-})
+    console.log('unhandledRejection: \n\n', err.stack);
+});
 
-async function createUser(u){
-    return await u.save();
+// Use ejs for templating, with the default directory /views
+app.set('view engine', 'ejs');
+
+app.use(flash());
+
+// Read cookies, required for sessions
+app.use(cookieParser('ilovethenodejs'));
+
+// Get POST/PUT body information (e.g., from html forms like login)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// In-memory session support, required by passport.session()
+app.use(session({
+  secret: 'ilovethenodejs',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Use the passport middleware to enable passport
+app.use(passport.initialize());
+
+// Enable passport persistent sessions
+app.use(passport.session());
+
+// SETTING UP PASSPORT
+
+passport.serializeUser(nodeifyit(async (user) => user.email));
+
+passport.deserializeUser(nodeifyit(async (email) => {
+    return await User.findOne({email}).exec();
+}));
+
+// login strategy
+passport.use(new LocalStrategy({
+    // Use "email" field instead of "username"
+    usernameField: 'email'
+}, nodeifyit(async (email, password) => {
+    email = (email || '').toLowerCase();
+    // get user from db
+    let user = await User.promise.findOne({email});
+    if (email !== user.email) {
+        return [false, {message: 'Invalid username'}];
+    }
+    let hash = (await crypto.promise.pbkdf2(password, SALT, 4096, 512, 'sha256')).toString('hex');
+    if (hash != user.password) {
+       return [false, {message: 'Invalid password'}]
+    }
+    return user;
+}, {spread: true})));
+
+
+// signup strategy
+passport.use('local-signup', new LocalStrategy({
+   // Use "email" field instead of "username"
+   usernameField: 'email',
+   passReqToCallback: true
+}, nodeifyit(async (req, email, password) => {
+    email = (email || '').toLowerCase()
+    let username = req.body.username;
+    // Is the email taken?
+    // if (await User.promise.findOne({email})) {
+    //     return [false, {message: 'That email is already taken.'}];
+    // }
+    
+    let user = new User();
+    if (await user.getUserByUsernameOrEmail(username, email)) {
+        return [false, {message: 'That email or username is already taken.'}];
+    }
+
+    if (!await user.isValidPassword(password)) {
+        return [false, {message: 'Password must be alpha numeric'}];
+    }
+
+    // need to double check
+    if (!await user.isAlphnumeric(username)) {
+        return [false, {message: 'Password must be alpha numeric'}];
+    }
+
+    // create the user
+    user.email = email;
+    user.username = username;
+    let hash = (await crypto.promise.pbkdf2(password, SALT, 4096, 512, 'sha256')).toString('hex');
+    user.password = hash;
+    return await user.save();
+}, {spread: true})));
+
+
+// ROUTING
+
+// And add your root route after app.listen
+app.get('/', (req, res) => {
+    res.render('index.ejs', {message: req.flash('error')});
+});
+
+// process the login form
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/profile',
+    failureRedirect: '/',
+    failureFlash: true
+}));
+
+// process the signup form
+app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/',
+    failureFlash: true
+}));
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next()
+    res.redirect('/')
 };
 
-let promise = createUser(user);
-promise
-    .then(function(user){
-        console.log('user', user);
-        let val = user.validatePassword('test5D1234');
-        val.then( function(matched){
-            console.log('matched', matched);
-        });
-    })
-    .catch(function(err){
-        console.log('err',err);
-    });
+app.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile.ejs', {user: req.user});
+});
 
-// user.save(function (err, u) {
-//     console.log('saving');
-//     if (err){
-//         return console.error(err);
-//     }
-//     console.log(u);
-// });
+app.get('/logout', function(req, res){
+   req.logout();
+   res.redirect('/');
+});
 
-// User.findOne({email: 'dle@walmart.com'}, function(err, user){
-//     if (err){
-//         console.log('error',err);
-//     }
-//     if (user){
-//         console.log('finding');
-//         console.log(user);
-//         user.password = 'newpassword';
-//         user.save(function(err, u){
-//             if (err){
-//                 console.log('update err',err);
-//             }else{
-//                 console.log('update success', u);
-//             }
-//             mongoose.connection.close();
-//         });
-//     }
-// })
-
-// User.find({}, function(err, users){
-//     if (err){
-//         console.log('error',err);
-//     }
-//     console.log('finding all');
-//     users.map( (u) => {
-//         console.log(u);
-//     });
-// })
-
-// async function getUser(email){
-//     return await User.findOne({email});
-// }
-
-// async function getAllUsers(){
-//     return await User.find({});
-// }
-
-// async function getUsersByQuery(query){
-//     return await User.find( query );
-// }
-
-// let u = getUser('dle4@walmart.com');
-// u.then( (uu) =>{
-//     console.log('uu', uu);
-// });
-
-// let all = getAllUsers();
-// all.then( (uu) =>{
-//     console.log('uu', uu);
-// });
-
-// let queryOrUser = getUsersByQuery( {
-//     $or:[
-//         {email: 'dle@walmart.com'},
-//         {email: 'dle2@walmart.com'}
-//     ]
-// });
-
-// queryOrUser.then( (uu) =>{
-//     console.log('uu', uu);
-// });
-
-// let queryAndUser = getUsersByQuery(
-//     {
-//         email: 'dle@walmart.com',
-//         password: 'newpassword'
-//     });
-
-// queryAndUser.then( (uu) =>{
-//     console.log('uu', uu);
-// });
-
-// async function getUserByUsername(username){
-//     return await User.findOne( {username: new RegExp('^'+username+'$', "i") } );
-// }
-
-// function main(){
-//     async () => {
-//         let exist = await user.getUserByUsernameOrEmail('duylex', 'dle5@walmart.comx') != null;
-//         console.log('exist', exist);
-//     }();
-// }
-
-// main();
-
-
-//  let x = user.getUserByUsername('Duyle');
-// x.then(function( u ){
-//     console.log(u);
-// }).catch(function (e){
-//     console.log('errr',e.stack);
-// });
+// start server 
+app.listen(PORT, ()=> console.log(`Listening @ http://127.0.0.1:${PORT}`))
--- user.js
+++ user.js
@@ -3,11 +3,11 @@ let nodeify = require('nodeify');
 let crypto = require('crypto');
 
 let UserSchema = mongoose.Schema({
-    username: {
+    email: {
         type: String,
         required: true
     },
-    email: {
+    username: {
         type: String,
         required: true
     },
@@ -18,33 +18,34 @@ let UserSchema = mongoose.Schema({
 });
 
 UserSchema.methods.generateHash = async function(password){
-    // crypto.pbkdf2(password,'salt',4092,32,'sha256', function(err, key){
-    //     if (!err){
-    //         console.log(key.toString('hex'));
-    //     }
-    // });
-    // use bcrypt to call: bcrypt.promise.hash(password, 8);
     return await (password + '1234');
 };
 
 UserSchema.methods.validatePassword = async function(password){
-    // use bcrypt to call: bcrypt.promise.compare(password, this.password)
     return await password === this.password;
 };
 
-UserSchema.pre('save', function(callback){
-    nodeify(async () => {
-        if (!this.isModified('password')){
-            return callback;
-        }
-        this.password = await this.generateHash(this.password);   
+// UserSchema.pre('save', function(callback){
+//     nodeify(async () => {
+//         if (!this.isModified('password')){
+//             return callback;
+//         }
+//         this.password = await this.generateHash(this.password);   
 
-    }(), callback);
-});
+//     }(), callback);
+// });
 
-UserSchema.path('password').validate( (pwd) =>{
-    return pwd.length >= 4 && /[A-Z]/.test(pwd) && /[a-z]/.test(pwd) && /[0-9]/.test(pwd);
-});
+// UserSchema.path('password').validate( (pwd) =>{
+//     return pwd.length >= 4 && /[A-Z]/.test(pwd) && /[a-z]/.test(pwd) && /[0-9]/.test(pwd);
+// });
+
+UserSchema.methods.isValidPassword = async function (val){
+    return val.length >= 4 && /[A-Z]/.test(val) && /[a-z]/.test(val) && /[0-9]/.test(val);
+}
+
+UserSchema.methods.isAlphaNumeric = async function (val){
+    return /[A-Za-z0-9]/.test(val);
+}
 
 UserSchema.methods.getUserByUsername = async function (username){
     return await this.model('User').findOne( {username: new RegExp('^'+username+'$', "i") } );
@@ -62,10 +63,10 @@ UserSchema.methods.getUserByUsernameOrEmail = async function (username, email){
 }
 
 
-UserSchema.path('username').validate(function(username, callback) {
-    return this.model('User').findOne( {username: new RegExp('^'+username+'$', "i") } , "username", function (err, user) { 
-        callback(user == null);
-    });
-}, "Username already exist");
+// UserSchema.path('username').validate(function(username, callback) {
+//     return this.model('User').findOne( {username: new RegExp('^'+username+'$', "i") } , "username", function (err, user) { 
+//         callback(user == null);
+//     });
+// }, "Username already exist");
 
 module.exports = mongoose.model('User', UserSchema);
